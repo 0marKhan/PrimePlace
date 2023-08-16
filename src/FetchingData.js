154204839d7deb249d0ef12d24721f3b79f400dc
Omar Khan <omakhan98@gmail.com>
2023-08-16 14:57:47 +0500

    added new api key

--- src/FetchingData.js
+++ src/FetchingData.js
@@ -43,7 +43,7 @@ const FetchingData = ({ onDataFetched }) => {
       categoryExternalID: "4",
     },
     headers: {
-      "X-RapidAPI-Key": "ed7b85fe80msh77491ed2c4da0e0p17f269jsn8568f1656544",
+      "X-RapidAPI-Key": "83836b648amsh97b5e8ec6497369p134bb7jsna8e79cb8fed1",
       "X-RapidAPI-Host": "bayut.p.rapidapi.com",
     },
   };
@@ -63,7 +63,7 @@ const FetchingData = ({ onDataFetched }) => {
       categoryExternalID: "4",
     },
     headers: {
-      "X-RapidAPI-Key": "ed7b85fe80msh77491ed2c4da0e0p17f269jsn8568f1656544",
+      "X-RapidAPI-Key": "83836b648amsh97b5e8ec6497369p134bb7jsna8e79cb8fed1",
       "X-RapidAPI-Host": "bayut.p.rapidapi.com",
     },
   };
